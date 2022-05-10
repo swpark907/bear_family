b5a25cb9382c6df5f2904a7f3aaf0ad7e48358fb
swpark907 <76856321+swpark907@users.noreply.github.com>
2022-05-10 20:52:51 +0900

    #3-7 [Fix] ID_REGEX 조건 수정

--- frontend/src/constants/regex.js
+++ frontend/src/constants/regex.js
@@ -1,3 +1,3 @@
-export const ID_REGEX = /^[a-z0-9-_]{6,14}$/;
+export const ID_REGEX = /^[a-z0-9-_]{6,20}$/;
 export const PW_REGEX = /^(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
 export const EMAIL_REGEX = /^[a-z0-9_+.-]+@([a-z0-9-]+\.)+[a-z0-9]{2,4}$/
