cf3522df6763146c1aa06361832ba08f3eaa8392
swpark907 <76856321+swpark907@users.noreply.github.com>
2022-05-11 19:02:10 +0900

    #3-0 [Update] 파일이름 변경 (NameInput -> NickNameInput)

---
+++ frontend/src/components/Register/NickNameInput.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const NickNameInput = () => {
+  return (
+    <div className="nick-name-form">
+      <h3>닉네임</h3>
+      <div className="nick-name-input">
+        <input className="nick-name-input__input" type="text" />
+      </div>
+    </div>
+  );
+};
+
+export default NickNameInput;
