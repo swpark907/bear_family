1ee46da5a061594b78579c6d43dee8cd75d24801
swpark907 <76856321+swpark907@users.noreply.github.com>
2022-05-13 17:47:22 +0900

    #3-0 [Update] 제목을 Title 컴포넌트로 변경

--- frontend/src/pages/Register/Register.jsx
+++ frontend/src/pages/Register/Register.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import RegisterForm from "../../components/Register/RegisterForm";
-import '../../styles/scss/main.scss';
-
+import "../../styles/scss/main.scss";
+import { Title } from "../../components/common";
 
 const Register = () => {
   return (
-    <section className="section__register">
-      <h2>회원가입</h2>
+    <section className="section-register">
+      <Title variant="primary" className="section-register__title">회원가입</Title>
       <RegisterForm />
     </section>
   );
