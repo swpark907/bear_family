945558753296d3ddedfc1c6422bcd8a72019eaca
swpark907 <76856321+swpark907@users.noreply.github.com>
2022-05-14 10:24:36 +0900

    #3-7 [Update] NickNameInput 컴포넌트 추가

--- frontend/src/components/Register/NickNameInput.jsx
+++ frontend/src/components/Register/NickNameInput.jsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Title } from "../common";
 
 const NickNameInput = () => {
+  const [nickName, setNickName] = useState("");
+  useEffect(() => {console.log(nickName)}, [nickName])
+
   return (
     <div className="nick-name-form">
-      <h3>닉네임</h3>
+      <Title variant={"tertiary"}>닉네임</Title>
       <div className="nick-name-input">
-        <input className="nick-name-input__input" type="text" />
+        <input
+          className="nick-name-input__input"
+          type="text"
+          onChange={(e) => {
+            setNickName(e.target.value);
+          }}
+        />
       </div>
     </div>
   );
