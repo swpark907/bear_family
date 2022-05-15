a705ab81305224b1ed40d019810955b84ddbca4b
swpark907 <76856321+swpark907@users.noreply.github.com>
2022-05-15 11:08:59 +0900

    #3-5 [Update] IdInput 컴포넌트 UI 및 유효성검사 구현 완료

--- frontend/src/components/Register/IdInput.jsx
+++ frontend/src/components/Register/IdInput.jsx
@@ -1,14 +1,76 @@
-import React from 'react';
+import React from "react";
+import { useState, useEffect } from "react";
+import { Title, Button } from "../common";
+import { ID_REGEX } from "../../constants/regex.js";
 
 const IdInput = () => {
+  const [중복확인문구, set중복확인문구] = useState("");
+  const [validChecked, setValidChecked] = useState(false);
+  const [is사용가능, setIs사용가능] = useState(false);
+  const [userId, setUserId] = useState("");
+  const [validUserId, setValidUserId] = useState(false);
+
+  useEffect(() => {
+    const result = ID_REGEX.test(userId);
+    result ? setValidUserId(true) : setValidUserId(false);
+    console.log(validUserId);
+  }, [userId]);
+
+  const onClickHandler = (e) => {
+    e.preventDefault();
+    // 버튼이 클릭되면 백엔드에 중복확인 한 후 받아온 응답에 따른 클래스명 조정
+
+    if(!validUserId){
+      set중복확인문구("아이디 형식에 맞지 않습니다.")
+      setIs사용가능(false);
+      return;
+    }
+
+    const result = true;
+    setValidChecked(true);
+    if (result) {
+      set중복확인문구("사용 가능한 아이디입니다.");
+      setIs사용가능(true);
+    } else {
+      set중복확인문구("중복된 아이디입니다.");
+      setIs사용가능(false);
+    }
+  };
+
   return (
-    <div>
-        <h3>아이디</h3>
-        <div>
-          <input type="text" />
-          <button>버튼입니다</button>
-        </div>
+    <div className="id-form">
+      <Title variant={"secondary"}>아이디</Title>
+      <div className="id-input">
+        <input
+          type="text"
+          className="id-input__input"
+          id="idInput"
+          placeholder="로그인 시 아이디 설정"
+          onChange={(e) => {
+            setUserId(e.target.value);
+          }}
+        />
+        <Button
+          className={"id-input__id-check-button"}
+          variant={"secondary"}
+          onClick={onClickHandler}
+        >
+          아이디 중복 확인
+        </Button>
       </div>
+      <label htmlFor="idInput" className="id-form__info">
+        6~20자 가능, 특수문자는 사용이 불가합니다.
+      </label>
+      <label
+        htmlFor="idInput"
+        className={
+          "id-form__valid" +
+          (validChecked && is사용가능 ? " valid" : " invalid")
+        }
+      >
+        {중복확인문구}
+      </label>
+    </div>
   );
 };
 
