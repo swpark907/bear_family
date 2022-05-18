ed2d7b5261d99d97faffe5bb324875361faf9121
swpark907 <76856321+swpark907@users.noreply.github.com>
2022-05-18 21:02:48 +0900

    #3-5 [Update] code-input timer display 조건 설정

--- frontend/src/components/Register/CodeInput.jsx
+++ frontend/src/components/Register/CodeInput.jsx
@@ -1,30 +1,56 @@
 import React from "react";
 import { Button } from "../common";
 
-const CodeInput = ({ validCheck, errMsg, inputActivate }) => {
+const CodeInput = ({
+  validCheck,
+  errCode,
+  errMsg,
+  codeInputActivate,
+  checkCodeHandler,
+}) => {
+  const classChanger = () => {
+    switch (errCode) {
+      case "hide":
+        return "hide";
+      case "timeOut" || "invalid":
+        return "invalid";
+      case "valid":
+        return "valid";
+      default:
+        console.log("classChagner 오류");
+        return;
+    }
+  };
+
   return (
-    <div className={"code-input" + (!inputActivate ? " hide" : " active")}>
+    <div className={"code-input" + (!codeInputActivate ? " hide" : " active")}>
       <div className="code-input__header">
         <div className="code-input__input-box">
           <input
             type="number"
             className="input-box__input"
             id="codeInput"
+            disabled={errCode === "valid" ? true : false}
           />
-          <div className="input-box__timer">03:00</div>
+          <div
+            className={
+              "input-box__timer" + (validCheck.emailCode ? " hide" : " active")
+            }
+          >
+            03:00
+          </div>
         </div>
         <Button
           className={"code-input__button"}
           variant={"secondary"}
+          onClick={checkCodeHandler}
         >
           인증번호 확인
         </Button>
       </div>
       <label
         htmlFor="codeInput"
-        className={
-          "code-input__info " + (!validCheck.emailCode ? "invalid" : "valid")
-        }
+        className={"code-input__info " + classChanger()}
       >
         {errMsg}
       </label>
