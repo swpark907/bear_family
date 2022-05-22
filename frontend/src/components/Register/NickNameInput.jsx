02008ff07d304548640697796eaa5faa3b029e25
swpark907 <76856321+swpark907@users.noreply.github.com>
2022-05-22 22:46:32 +0900

    #3-7 [Update] 닉네임 유효성검사 로직 추가

--- frontend/src/components/Register/NickNameInput.jsx
+++ frontend/src/components/Register/NickNameInput.jsx
@@ -1,10 +1,23 @@
 import React, { useEffect, useState } from "react";
 import { Title } from "../common";
+import { NICKNAME_REGEX } from "../../constants/regex";
 
-const NickNameInput = ({ userInfo, setUserInfo }) => {
+const NickNameInput = ({
+  userInfo,
+  setUserInfo,
+  validCheck,
+  setValidCheck,
+}) => {
   const nickNameInputHandler = ({ target }) => {
     setUserInfo({ ...userInfo, userName: target.value });
   };
+
+  useEffect(() => {
+    const result = NICKNAME_REGEX.test(userInfo.userName);
+    result
+      ? setValidCheck({ ...validCheck, userName: true })
+      : setValidCheck({ ...validCheck, userName: false });
+  }, [userInfo.userName]);
 
   return (
     <div className="nick-name-form">
@@ -12,9 +25,21 @@ const NickNameInput = ({ userInfo, setUserInfo }) => {
       <div className="nick-name-input">
         <input
           className="nick-name-input__input"
+          id="userNameInput"
           type="text"
           onChange={nickNameInputHandler}
         />
+        <label
+          htmlFor="userNameInput"
+          className={"nick-name-input__info"}
+        >한글, 영문, 숫자 가능 2-8글자</label>
+        <label
+          htmlFor="userNameInput"
+          className={
+            "nick-name-input__info" +
+            (userInfo.userName && !validCheck.userName ? " invalid" : " hide")
+          }
+        >닉네임 형식에 맞지 않습니다.</label>
       </div>
     </div>
   );
