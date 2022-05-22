9253ff2fe7103c47f3920bf13a80c77d1f622bd8
swpark907 <76856321+swpark907@users.noreply.github.com>
2022-05-22 22:46:10 +0900

    #3-7 [Update] 닉네임 유효성검사 정규표현식 추가

--- frontend/src/constants/regex.js
+++ frontend/src/constants/regex.js
@@ -1,3 +1,4 @@
 export const ID_REGEX = /^[a-z0-9-_]{6,20}$/;
 export const PW_REGEX = /^(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
 export const EMAIL_REGEX = /^[a-z0-9_+.-]+@([a-z0-9-]+\.)+[a-z0-9]{2,4}$/
+export const NICKNAME_REGEX = /^[가-힣a-zA-Z0-9]{2,8}$/
