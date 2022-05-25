95f38f6d182d0b26c333cf513c5857b987586a4d
swpark907 <76856321+swpark907@users.noreply.github.com>
2022-05-25 17:15:59 +0900

    #2-0 [Update] 로그인 라우터 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,18 @@ import React from "react";
 import "./App.scss";
 import { Routes, Route } from "react-router-dom";
 import Register from "./pages/Register/Register";
+import RegisterSuccess from "./pages/Register/RegisterSuccess";
+import Login from "./pages/Login/Login";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='register' element={<Register />} />
+        <Route path="register">
+          <Route path="" element={<Register />} />
+          <Route path="success" element={<RegisterSuccess />} />
+        </Route>
+        <Route path="login" element={<Login />} />
       </Routes>
     </div>
   );
