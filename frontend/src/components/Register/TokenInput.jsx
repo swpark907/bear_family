539dd7a270f3cc755b68fc221c8c87f9be4a5fd5
swpark907 <76856321+swpark907@users.noreply.github.com>
2022-05-27 11:07:24 +0900

    #3-3 [Update] token-input__info 클래스명 핸들 로직 변경 (함수 사용 => useEffect) 및 인증 성공시 버튼 disabled 조건 추가

--- frontend/src/components/Register/TokenInput.jsx
+++ frontend/src/components/Register/TokenInput.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "../common";
 import useTimer from "../../hooks/useTimer";
 
 const TokenInput = ({
   validCheck,
-  errToken,
+  errCode,
   errMsg,
   tokenInputActivate,
   checkTokenHandler,
@@ -14,22 +14,33 @@ const TokenInput = ({
   timerMin,
   timerSec,
 }) => {
-  const classChanger = () => {
-    switch (errToken) {
+  const [tokenInputClass, setTokenInputClass] = useState("");
+
+  useEffect(() => {
+    console.log(errCode);
+    switch (errCode) {
       case "hide":
-        return "hide";
-      case "timeOut" || "invalid":
-        return "invalid";
+        setTokenInputClass("hide");
+        return;
+      case "timeOut":
+        setTokenInputClass("invalid");
+        return;
+      case "invalid":
+        setTokenInputClass("invalid");
+        return;
       case "valid":
-        return "valid";
+        setTokenInputClass("valid");
+        return;
       default:
         console.log("classChagner 오류");
         return;
     }
-  };
+  }, [errCode]);
 
   return (
-    <div className={"token-input" + (!tokenInputActivate ? " hide" : " active")}>
+    <div
+      className={"token-input" + (!tokenInputActivate ? " hide" : " active")}
+    >
       <div className="token-input__header">
         <div className="token-input__input-box">
           <input
@@ -37,7 +48,7 @@ const TokenInput = ({
             className="input-box__input"
             id="tokenInput"
             disabled={
-              errToken === "valid" || errToken === "timeOut" ? true : false
+              errCode === "valid" || errCode === "timeOut" ? true : false
             }
             onChange={({ target }) => setCheckToken(target.value)}
           />
@@ -53,13 +64,14 @@ const TokenInput = ({
           className={"token-input__button"}
           variant={"secondary"}
           onClick={checkTokenHandler}
+          disabled={validCheck.emailToken ? true : false}
         >
           인증번호 확인
         </Button>
       </div>
       <label
         htmlFor="tokenInput"
-        className={"token-input__info " + classChanger()}
+        className={"token-input__info " + tokenInputClass}
       >
         {errMsg}
       </label>
