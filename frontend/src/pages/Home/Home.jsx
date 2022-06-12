6c472701df78b52a2d1adf0fd9b520efbc7441c3
swpark907 <76856321+swpark907@users.noreply.github.com>
2022-06-12 22:28:11 +0900

    #4-1,#4-3,#4-7 [Add] Home 페이지의 Carousel, Header UI 업데이트

---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from '../../components/Home/Header';
+import Carousel from '../../components/Home/Carousel';
+import Target from '../../components/Home/Target';
+
+const Home = () => {
+  return (
+    <section className='section-home'>
+      <Header />
+      <Carousel />
+      <Target />
+      history
+    </section>
+  );
+};
+
+export default Home;
