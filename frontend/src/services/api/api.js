c0d734ca8a4b4b5cbbec2f3687eca6e49fd8ec06
swpark907 <76856321+swpark907@users.noreply.github.com>
2022-06-19 17:52:45 +0900

    #0-0 [Update] api 통신 instance 파일 생성

---
+++ frontend/src/services/api/api.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+const URL = process.env.REACT_APP_BASE_URL;
+
+export const api = () => {
+  const instance = axios.create({
+    baseURL: URL,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  return instance;
+};
+
+// 매 페이지 랜더링마다 실행되는 함수
+export const apiWithToken = async () => {
+  const accessToken = localStorage.getItem("access-token");
+
+  const instance = axios.create({
+    baseURL: URL,
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  return instance;
+};
+
+
