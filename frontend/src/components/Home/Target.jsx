f35b50be5139d0f292d8b2467d33d66c60badf51
swpark907 <76856321+swpark907@users.noreply.github.com>
2022-06-22 16:18:59 +0900

    #4-7 [Fix] 클래스 명 변경 (random-info -> target)

--- frontend/src/components/Home/Target.jsx
+++ frontend/src/components/Home/Target.jsx
@@ -3,7 +3,7 @@ import { Button } from '../common';
 
 const RandomInfo = () => {
   return (
-    <div className='home__random-info'>
+    <div className='home__target'>
       <p>목표가 및 잔액</p>
       <Button variant={'tertiary'}>더 보러가기</Button>
     </div>
