3fef73a2c24245b21d6b08f28f95f0561f40a876
용보 심 <dydqh2648@datasolution.kr>
2022-07-19 17:12:58 +0900

    #0-0 [Add] Swagger 사용, 카테고리까지 정리 완료 / DTO를 사용한 모델 폴더구조 정리

---
+++ backend/src/main/java/dragonb/bearfamily/backend/model/category/CategoryDTO.java
@@ -0,0 +1,11 @@
+package dragonb.bearfamily.backend.model.category;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CategoryDTO {
+    private String name;
+    private String color;
+}
