ea19d967dadd47d450f8086f650c67057faf7b0e
swpark907 <76856321+swpark907@users.noreply.github.com>
2022-09-13 23:23:37 +0900

    #3-4 [Update] useApi 시간이 만료되면 새로운 토큰 발행하는 로직

--- frontend/src/hooks/useApi.js
+++ frontend/src/hooks/useApi.js
@@ -1,9 +1,12 @@
 import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { apiWithToken, api } from "../services/api/api";
+import { logout } from "../reducers/loginReducer";
 
-const useApi = ({ url }) => {
+const useApi = ({ url, method }) => {
   const [pageData, setPageData] = useState(null);
+
+  const dispatch = useDispatch();
 
   const getUserInfo = useSelector((state) => state.userInfoReducer);
 
@@ -11,26 +14,40 @@ const useApi = ({ url }) => {
   const currentTime = Date.now();
   const remainTime = expiredTime - currentTime;
 
+  
+  // 이 부분이 매 페이지 랜더링 될 때 실행되어야함.
+  // 요청 method가 뭔지 받아서 쓸것인가(switch)?
   const getNewToken = async () => {
     const instance = apiWithToken();
-    if (remainTime < 10000) {
+
+    console.log(expiredTime, currentTime);
+    if (remainTime && remainTime < 10000) {
       const userInfo = {
         refreshToken: getUserInfo.refreshToken,
       };
-      const { data } = await instance.post("/refresh", userInfo);
-      console.log(data);
-      if (data.response === "success") {
-        localStorage.setItem("access-token", data.data);
+      try {
+        const { data } = await instance.post("/refresh", userInfo);
+        console.log(userInfo);
+        if (data.response === "success") {
+          localStorage.setItem("access-token", data.data);
+          console.log("새로운 토큰 발행");
+        }        
+      } catch (e) {
+        dispatch(logout);
       }
+    } else {
+      console.log("아직 만료되지 않은 토큰입니다.");
     }
   };
+
+  // 처음 랜더링 시 로그인이 자동으로 되어야함.
+  // 로그인이 되어야 하는 조건
+  // 리프레시토큰, 엑세스토큰을 통해 userInfo가 store에 담겨야 함.
 
   const getPageData = async () => {
     await getNewToken();
     const instance = apiWithToken();
-    const response = await instance.post(url, {
-      id: getUserInfo.userInfo.id,
-    });
+    const response = await instance.get(url);
     console.log(response);
     setPageData(response.data); // 콘솔 찍어보고 수정하기
   };
