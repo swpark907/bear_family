0350d6112e5e8fedb0e6d99362c77d33434717a4
swpark907 <76856321+swpark907@users.noreply.github.com>
2022-09-13 23:29:28 +0900

    #6-2 [Update] 일별 데이터 받아오기 및 onClick 시 active UI 개선 업데이트

--- frontend/src/components/Calendar/CalendarDate.jsx
+++ frontend/src/components/Calendar/CalendarDate.jsx
@@ -1,5 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { calendarReducer, changeDate } from "../../reducers/calendarReducer";
+import { apiWithToken } from "../../services/api/api";
 import { Button } from "../common";
 
 const CalendarDate = () => {
@@ -11,14 +14,40 @@ const CalendarDate = () => {
   const LASTDATE = LAST.getDate();
   const DAYOFFIRST = new Date(YEAR, MONTH - 1, 1).getDay();
 
-  const [currentYear, setCurrentYear] = useState(YEAR);
-  const [currentMonth, setCurrentMonth] = useState(MONTH);
+  const [currentYear, setCurrentYear] = useState(YEAR); // 현재 연도
+  const [currentMonth, setCurrentMonth] = useState(MONTH); // 현재 달
   const [currentDate, setCurrentDate] = useState(DATE);
+
+  const [currentDataset, setCurrentDataset] = useState(0); // 0을 넣는게 맞나?
 
   const [dateList, setDateList] = useState([]);
 
-  // 첫번째 날의 요일이 목요일(4) 이상이면 6줄, 이하이면 5줄
-  // 이번달의 첫번째 날을 구한다.
+  const dateRefs = useRef([]);
+
+  const dispatch = useDispatch();
+
+  useEffect(async () => {
+    try {
+      const instance = apiWithToken();
+      const responseList = await instance.get("/api/ledger/items/date", {
+        params: {
+          year: currentYear,
+          month: currentMonth,
+          date: currentDate,
+        },
+      });
+      const data = responseList.data.data;
+      dispatch(changeDate(data));
+
+      const responseTotal = await instance.get("/api/ledger/items/groupbymonth")
+    } catch (e) {
+      console.log("날짜 데이터를 받아올 때 생긴 오류입니다", e);
+    }
+  }, [currentDataset]);
+
+  useEffect(() => {
+    setCurrentDataset(formChanger(NOW));
+  }, []);
 
   const getCalendarLine = () => {
     // 현재 달의 첫째날의 요일
@@ -40,26 +69,76 @@ const CalendarDate = () => {
     getCalendarLine();
   }, [currentMonth]);
 
-  // useEffect(() => {
-  //   console.log(dateList);
-  // }, [dateList]);
-
   const goToNextMonth = () => {
-    setCurrentMonth(currentMonth + 1);
+    if (currentMonth >= 12) {
+      setCurrentMonth(1);
+      setCurrentYear(currentYear + 1);
+    } else {
+      setCurrentMonth(currentMonth + 1);
+    }
   };
 
   const goToPrevMonth = () => {
-    setCurrentMonth(currentMonth - 1);
+    if (currentMonth <= 1) {
+      setCurrentMonth(12);
+      setCurrentYear(currentYear - 1);
+    } else {
+      setCurrentMonth(currentMonth - 1);
+    }
+  };
+
+  const formChanger = (date) => {
+    const year = date.getFullYear().toString();
+    const month =
+      date.getMonth() + 1 < 10
+        ? "0" + (date.getMonth() + 1).toString()
+        : (date.getMonth() + 1).toString();
+    const day =
+      date.getDate() < 10
+        ? "0" + date.getDate().toString()
+        : date.getDate().toString();
+    return Number(year + month + day);
+  };
+
+  const dateOnclick = (e) => {
+    const target = e.currentTarget;
+
+    const dateData = target.dataset.date;
+    const year = Number(dateData.substr(0, 4));
+    const month = Number(dateData.substr(4, 2));
+    const date = Number(dateData.substr(6));
+    setCurrentYear(Number(year));
+    setCurrentMonth(Number(month));
+    setCurrentDate(Number(date));
+    setCurrentDataset(dateData);
+  };
+
+  const ledger = {
+    // data 자체를 좀 거대하게 미리 받아놓는다
+    // 전부 받는다 전역으로 저장해놓을 수 있는 무언가
+    // 달 옮길때마다 통신을 할거냐 => 네이버의 경우 이렇지 않은 듯
+    // 생각을 좀 더 해보겠음
+    // 캐러셀로 이전 다음거는 무조건 받아놔야되
+    // 생성은 년, 월, 일
+    // created: 'Date.parse() 형태로'
+    // 날짜를
   };
 
   return (
     <ul className="template__date-container">
+      <span></span>
       {dateList.map((date, key) => {
         return (
           <li
-            className={"date-container__date"}
+            className={
+              "date-container__date" +
+              (formChanger(date) === Number(currentDataset) ? " selected" : "")
+            }
             key={key}
-            id={Date.parse(date)}
+            // id={Date.parse(date)}
+            data-date={formChanger(date)}
+            onClick={dateOnclick}
+            ref={(el) => dateRefs.current.push(el)}
           >
             <div
               className={
@@ -74,11 +153,14 @@ const CalendarDate = () => {
                   : "")
               }
             >
-              <span className="date">{date.getDate()}</span>
+              <div className="date">{date.getDate()}</div>
+              <div className="usage">-10000</div>
+              <div className="tags"></div>
             </div>
           </li>
         );
       })}
+      {currentYear}년 {currentMonth} 월
       <Button onClick={goToPrevMonth}>이전</Button>
       <Button onClick={goToNextMonth}>다음</Button>
     </ul>
