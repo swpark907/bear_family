5245a366533942e8f87f5eb251df4e6b54c1865c
swpark907 <76856321+swpark907@users.noreply.github.com>
2022-09-13 23:31:07 +0900

    #2-0 [Update] userInfo reducer를 persist에 추가

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from "redux";
 import { loginReducer, userInfoReducer } from "./loginReducer";
 import { loadingReducer } from "./loadingReducer";
+import { calendarReducer } from "./calendarReducer";
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
@@ -9,7 +10,7 @@ const persistConfig = {
   // localStorage에 저장합니다.
   storage,
   // auth, board, studio 3개의 reducer 중에 auth reducer만 localstorage에 저장합니다.
-  whitelist: ["loginReducer"],
+  whitelist: ["loginReducer", "userInfoReducer"],
   // blacklist -> 그것만 제외합니다
 };
 
@@ -17,6 +18,7 @@ const rootReducer = combineReducers({
   loadingReducer,
   loginReducer,
   userInfoReducer,
+  calendarReducer,
 });
 
 export default persistReducer(persistConfig, rootReducer);
